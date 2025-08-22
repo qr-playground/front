27233fa48bf110868374f4f44832b1edea7f9cc5
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-08-23 02:18:23 +0900

    Fix: 소개 페이지 문구 수정

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <footer className="footer">
         <div className="footer-content">
           <p>
-            개선 요청, 오류 제보 시 소정의 상품을 드립니다.
+            개선 사항,오류 제보 시 소정의 상품을 드립니다.
             <br />
             📧 이메일: suhwani.dev@gmail.com
           </p>
