205e58e79ff5b1d77a8a89c2868a474507e4f6d9
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-09-10 02:18:59 +0900

    Feat: 구글폼 링크 추가

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -14,9 +14,16 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <footer className="footer">
         <div className="footer-content">
           <p>
-            개선 사항,오류 제보 시 소정의 상품을 드립니다.
+            개선 사항/오류 제보는 아래 구글폼으로 부탁드립니다. (참여자 중
+            소정의 상품 증정)
             <br />
-            📧 이메일: suhwani.dev@gmail.com
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdgNG61y42xsheoVQgxTRU-PwjvhE_3YTUB-zV-YRW5EWxkdA/viewform?usp=header"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              📝 구글폼 작성하기
+            </a>
           </p>
           <p style={{ marginTop: 8 }}>
             <a
@@ -32,7 +39,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-             🔗 GitHub 저장소
+              🔗 GitHub 저장소
             </a>
           </p>
         </div>
